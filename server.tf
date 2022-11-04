3bb1757bf790ca2b4abdf93c417d399659cd7c58
Meghraj Agnihotri <38850672+meghraj-agnihotri@users.noreply.github.com>
2022-11-04 15:13:22 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,13 +1,13 @@
 provider "aws" {
 region = "us-east-2"
 }
-resource "aws_instance" "my-meghraj" {
+resource "aws_instance" "my-meghraj2" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
   key_name = "Meghraj_jenkins"
 
   tags = {
-    Name = "Meghraj-server"
+    Name = "Meghraj-server2"
     env = "Development"
     owner = "Meghraj"
   }
